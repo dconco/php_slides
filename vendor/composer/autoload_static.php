66fa1b23f850d1d3685e549c7c579a809906d05b
dconco <onipededavid208@gmail.com>
2023-12-02 18:02:31 +0100

    feat: Add CORS support for cross-origin requests
    The commit adds Cross-Origin Resource Sharing (CORS) support to allow
    cross-origin requests from any origin. It includes the necessary HTTP
    headers and methods to handle preflight requests. This change enables
    the web application to interact with resources on different domains,
    improving interoperability and enhancing functionality. The 'cor.php'
    file, which previously contained CORS configuration, has been removed.
    Additionally, the autoload files and autoload static classes have been
    updated to include the new 'cors.php' file from the 'Models' directory.
    
    Closes #1234

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -10,6 +10,7 @@ class ComposerStaticInitb42587e9b45336e82a2edb8da245e106
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
+        'f034ca428f0ef14c23cbebabb510b6c8' => __DIR__ . '/../..' . '/Models/cors.php',
     );
 
     public static $prefixLengthsPsr4 = array (
